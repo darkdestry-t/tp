b90d8ad1650ab72a30bc692dfa8c7c1df96d2141
darkdestry-t <tanki123+temp@hotmail.com>
2021-03-04 03:22:03 +0800

    feat: Add new command 'remarks'

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -34,6 +34,8 @@ public class CommandTestUtil {
     public static final String VALID_EMAIL_BOB = "bob@example.com";
     public static final String VALID_ADDRESS_AMY = "Block 312, Amy Street 1";
     public static final String VALID_ADDRESS_BOB = "Block 123, Bobby Street 3";
+    public static final String VALID_REMARK_AMY = "Why is this mod so stupid";
+    public static final String VALID_REMARK_BOB = "Pls help me kill myself";
     public static final String VALID_TAG_HUSBAND = "husband";
     public static final String VALID_TAG_FRIEND = "friend";
 
